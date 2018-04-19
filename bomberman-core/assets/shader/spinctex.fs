d0a8aacfe236d8897d127f2beb5b9b0adcf4862f
Michael Kirchmann <info@michaelkirchmann.de>
2018-04-19 22:52:34 +0200

    make float

--- bomberman-core/assets/shader/spinctex.fs
+++ bomberman-core/assets/shader/spinctex.fs
@@ -3,6 +3,6 @@ uniform sampler2D u_texture;
 varying float intensity;
 varying vec2 texCoords;
 void main() {
-vec4 tc=texture2D(u_texture, vec2(texCoords.x, 1-texCoords.y));
+vec4 tc=texture2D(u_texture, vec2(texCoords.x, 1f-texCoords.y));
 gl_FragColor =  vec4(intensity*tc.r,intensity*tc.g,intensity*tc.b,tc.a);
 }
